2133c7535e28dec58d6307843b5a9ff76d4e172a
Luis Antunes <luis.antunes@thomsonreuters.com>
2018-10-29 16:43:14 -0400

    initial commit

---
+++ word_mcts_demo.py
@@ -0,0 +1,32 @@
+from nlgmcts import *
+from nltk import *
+from nltk.corpus import brown
+from nltk.corpus import shakespeare
+import string
+
+
+if __name__ == '__main__':
+
+    num_simulations = 10000
+    alphabet = list(string.ascii_lowercase)
+    word_length = 5
+    start_state = ['t']
+
+    # words = [w.lower() for w in brown.words()]
+    all_words = []
+    for book in shakespeare.fileids():
+        all_words.extend(shakespeare.words(book))
+    words = set([w.lower() for w in all_words])
+
+    eval_function = lambda word : 1 if word in words else 0
+
+    mcts = WordMCTS(alphabet, word_length, eval_function)
+    state = start_state
+
+    while len(state) < word_length:
+        state = mcts.search(state, num_simulations)
+        print(state)
+
+    generated_word = ''.join(state)
+    print("generated word: %s" % generated_word)
+    print("is in corpus: %s" % (generated_word in words))
